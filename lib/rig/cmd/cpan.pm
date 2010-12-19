68dcf6ccf5425c506a2471edc42486b53d115276
Rodrigo de Oliveira Gonzalez <rodrigo@cpan.org>
2010-12-19 23:37:27 +0000

    Import of RODRIGO/rig-0.01_01 from CPAN.
    gitpan-cpan-distribution: rig
    gitpan-cpan-version:      0.01_01
    gitpan-cpan-path:         RODRIGO/rig-0.01_01.tar.gz
    gitpan-cpan-author:       RODRIGO
    gitpan-cpan-maturity:     developer

---
+++ lib/rig/cmd/cpan.pm
@@ -0,0 +1,55 @@
+package rig::cmd::cpan;
+BEGIN {
+  $rig::cmd::cpan::VERSION = '0.01_01';
+}
+use strict;
+use CPAN;
+use CPAN::Shell;
+use rig '-load';
+
+sub run {
+    my $self = shift;
+    my $parser = $rig::opts{parser};
+    my $data = $parser->rc_parse;
+    #return unless ref $data eq 'HASH';
+    for my $task ( keys %$data ) {
+        print "Loaded $task...\n";
+        for my $module ( @{ $data->{$task} } ) {
+            ref $module eq 'HASH' and $module = (keys %$module)[0];
+            print "Installing $module...\n";
+            $self->_install_module($module);
+        }
+    }
+}
+
+sub _install_module {
+    my $self = shift;
+    my $module = shift;
+    CPAN::Shell->install( $module );
+}
+
+1;
+
+=head1 NAME
+
+rig::cmd::cpan - Command to install a rig with the cpan command line
+
+=head1 VERSION
+
+version 0.01_01
+
+=head1 SYNOPSYS
+
+	rigup cpan
+
+=head1 DESCRIPTION
+
+This is quite experimental yet.
+
+=head1 METHODS
+
+=head2 run
+
+Calls the CPAN shell to install rig modules. 
+
+=cut 
---
+++ t/imports.t
@@ -0,0 +1,24 @@
+BEGIN {
+    sub rig::task::t_imports::rig {
+        { use => [
+            { 'List::Util'=> [ 'sum','max' ] },
+            { 'List::MoreUtils'=> [ 'any','firstval' ] }
+        ] }
+    };
+}
+
+use Test::More;
+
+eval { require List::Util };
+plan skip_all => "List::Util not installed" if $@; 
+
+use FindBin '$Bin';
+use rig -file => $Bin . '/perlrig';
+use rig 't_imports';
+
+is( sum(1..10), 55, 'sum' );
+is( max(1..10), 10, 'max' );
+is( do { firstval { $_ eq 10 } 1..20 } , 10, 'firstval' );
+ok( do { any { $_ eq 10 } 1..20 }, 'any' );
+
+done_testing;
